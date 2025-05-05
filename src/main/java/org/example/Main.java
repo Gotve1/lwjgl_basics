e8cc995be7fb7ce1050945fc5c0a7f609a941ba8
Sunrise <akselvolkov40@gmail.com>
2025-05-05 11:36:52 +0500

    minor changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
 package org.example;
 
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.system.MemoryStack;
 
 import java.nio.IntBuffer;
 
-import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL.createCapabilities;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Main {
@@ -29,40 +30,40 @@ public class Main {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
 
-        GLFW.glfwDefaultWindowHints();
-        GLFW.glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        GLFW.glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
         window = GLFW.glfwCreateWindow(600, 400, "OpenGL Window", NULL, NULL);
         if(window == NULL) {
             throw new RuntimeException("Failed to create the GLFW window");
         }
 
-        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {});
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {});
 
         try(MemoryStack stack = MemoryStack.stackPush()) {
             IntBuffer pWidth = stack.mallocInt(1);
             IntBuffer pHeight = stack.mallocInt(1);
 
-            GLFW.glfwGetWindowSize(window, pWidth, pHeight);
+            glfwGetWindowSize(window, pWidth, pHeight);
 
             GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
 
-            GLFW.glfwSetWindowPos(window,(vidMode.width() - pWidth.get(0)) / 2, (vidMode.height() - pHeight.get(0)) / 2);
+            glfwSetWindowPos(window,(vidMode.width() - pWidth.get(0)) / 2, (vidMode.height() - pHeight.get(0)) / 2);
 
-            GLFW.glfwMakeContextCurrent(window);
-            GLFW.glfwSwapInterval(1);
-            GLFW.glfwShowWindow(window);
+            glfwMakeContextCurrent(window);
+            glfwSwapInterval(1);
+            glfwShowWindow(window);
         }
 
         init();
         loop();
 
         Callbacks.glfwFreeCallbacks(window);
-        GLFW.glfwDestroyWindow(window);
+        glfwDestroyWindow(window);
 
-        GLFW.glfwTerminate();
-        GLFW.glfwSetErrorCallback(null).free();
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     public void init() {
@@ -70,14 +71,14 @@ public class Main {
     }
 
     public void loop() {
-        GL.createCapabilities();
+        createCapabilities();
 
         while(!GLFW.glfwWindowShouldClose(window)) {
-            GL11.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            GLFW.glfwSwapBuffers(window);
+            glfwSwapBuffers(window);
 
-            GLFW.glfwPollEvents();
+            glfwPollEvents();
         }
     }
 
